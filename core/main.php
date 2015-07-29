01e22d1b06fb048283aac57dab3e408f4ec41891
JavaBird <baidu211@vip.qq.com>
2015-07-29 23:14:08 +0800

    modify css

--- core/main.php
+++ core/main.php
@@ -1,9 +1,9 @@
 <?php
 $PHP_SELF=$_SERVER['PHP_SELF'];
 
-define('BasePath', 'http://'.$_SERVER['HTTP_HOST'].substr($PHP_SELF,0,strrpos($PHP_SELF,'/')+1));
-
 require_once  'setup.php';
+
+define('BasePath', 'http://'.$_SERVER['HTTP_HOST'].'/');
 
 $smarty = new SmartyCms();
 
